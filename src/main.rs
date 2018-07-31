c35eb24709cd50803daafb12c6c8b063edd72c92
Dan Bruder <ddbruder@gmail.com>
2018-07-31 16:48:47 -0400

    got template variables set up

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate rocket;
 extern crate rocket_contrib;
 #[macro_use]
 extern crate serde_derive;
+use rocket::response::{Flash, Redirect};
 use rocket_contrib::{Json, Template, Value};
 mod client;
 use client::{Client, NewClient};
@@ -13,7 +14,7 @@ extern crate diesel;
 extern crate r2d2;
 extern crate r2d2_diesel;
 mod db;
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 mod schema;
 
 #[post("/", data = "<client>")]
@@ -45,17 +46,33 @@ fn delete(id: i32, connection: db::Connection) -> Json<Value> {
     Json(json!({ "success": Client::delete(id, &connection) }))
 }
 
+// Registration
+#[get("/")]
+fn home() -> Template {
+    let mut context = BTreeMap::new();
+    context.insert("register_path".to_string(), "/register".to_string());
+    Template::render("index", context)
+}
+
+// Registration
 #[get("/register")]
 fn register() -> Template {
-    let context: HashMap<char, usize> = HashMap::new();
+    let mut context = BTreeMap::new();
+    context.insert("world".to_string(), "世界!".to_string());
+    context.insert("register_path".to_string(), "/register".to_string());
     Template::render("register", context)
+}
+
+#[post("/register")]
+fn register_post() -> Flash<Redirect> {
+    Flash::new(Redirect::to("/"), "suggestion", "Try this out!")
 }
 
 fn main() {
     rocket::ignite()
         .manage(db::connect())
         .attach(Template::fairing())
-        .mount("/", routes![register])
+        .mount("/", routes![home, register, register_post])
         .mount("/clients", routes![create, read, update, delete])
         .launch();
 }
